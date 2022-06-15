4df0826c3cf11f927753ad3c49159ece57a846b3
s-ferrer <saraferrer@gmail.com>
2022-06-15 14:58:50 +0200

    Code Exercises 1.6.-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,76 @@
 import { useState } from "react";
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Header = (props) => <h1>{props.name}</h1>;
 
-  return <div>code here</div>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Statistic = ({ text, value }) => {
+  if (text === "positive") {
+    return (
+      <tr>
+        <td>
+          {text} {value} %{" "}
+        </td>
+      </tr>
+    );
+  }
+  return (
+    <tr>
+      <td>
+        {text} {value}{" "}
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = ({ clicks }) => {
+  const total = clicks.good + clicks.neutral + clicks.bad;
+  const average = (clicks.good * 1 + clicks.bad * -1) / total;
+  const positive = clicks.good * (100 / total);
+
+  if (total === 0) {
+    return <div> No feedbacks</div>;
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <Statistic text="good" value={clicks.good} />
+          <Statistic text="neutral" value={clicks.neutral} />
+          <Statistic text="bad" value={clicks.bad} />
+          <Statistic text="all" value={total} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleGoodClick = () => setClicks({ ...clicks, good: clicks.good + 1 });
+
+  const handleNeutralClick = () =>
+    setClicks({ ...clicks, neutral: clicks.neutral + 1 });
+
+  const handleBadClick = () => setClicks({ ...clicks, bad: clicks.bad + 1 });
+
+  return (
+    <div>
+      <Header name="Customer feedback" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+      <Header name="Statistics" />
+      <Statistics clicks={clicks} />
+    </div>
+  );
 };
 
 export default App;
