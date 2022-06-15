38415c4c746ccccdd7d687d1d6d80b4484ea1f8e
s-ferrer <saraferrer@gmail.com>
2022-06-15 16:18:03 +0200

    Code Exercises 1.12.-1.14

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -61,6 +61,26 @@ const App = () => {
 
   const handleBadClick = () => setClicks({ ...clicks, bad: clicks.bad + 1 });
 
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+  const [selected, setSelected] = useState(0);
+
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+  const user = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+  const randomText = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+
   return (
     <div>
       <Header name="Customer feedback" />
@@ -69,6 +89,15 @@ const App = () => {
       <Button onClick={handleBadClick} text="bad" />
       <Header name="Statistics" />
       <Statistics clicks={clicks} />
+      <Header name="Anecdote of the day" />
+      {anecdotes[selected]}
+      <br></br>
+      <Button onClick={user} text="vote" />
+      <Button onClick={randomText} text="next" />
+      <h1>Anecdote with more votes</h1>
+      {anecdotes[points.indexOf(Math.max(...points))]}
+      <br></br>
+      has a total of {Math.max(...points)} votes
     </div>
   );
 };
